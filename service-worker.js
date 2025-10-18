33c973b40dbe359c11c9884cf27a342815bb8fab
ptkhaimaheniatrans-ops <ptkhaimaheniatrans@gmail.com>
2025-10-18 13:56:35 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,39 @@
-const CACHE_NAME = "carwashdb-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/assets/icons/icon-192.png",
-  "/assets/icons/icon-512.png",
-  "/assets/sound/klik.mp3",
-  "/assets/sound/success.mp3",
-  "/assets/sound/error.mp3",
-  "/assets/sound/welcome.mp3"
+const CACHE_NAME = "carwash-db-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.jpg",
+  "./assets/sound/klik.mp3",
+  "./assets/sound/success.mp3",
+  "./assets/sound/error.mp3",
+  "./assets/sound/welcome.mp3"
 ];
 
 // Install Service Worker
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 // Activate Service Worker
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
-  self.clients.claim();
 });
 
-// Fetch Files
-self.addEventListener("fetch", (event) => {
+// Fetch Offline Support
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
