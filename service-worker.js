aeef65f115afaef21bd63ff79f7d6f1cc6185404
ptkhaimaheniatrans-ops <ptkhaimaheniatrans@gmail.com>
2025-10-18 13:44:04 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,49 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('carwash-cache-v1').then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/style.css',
-      '/script.js',
-      '/assets/icons/icon-192.png.PNG',
-      '/assets/icons/icon-512.png.jpeg',
-      '/assets/sounds/klik.mp3',
-      '/assets/sounds/success.mp3',
-      '/assets/sounds/error.mp3',
-      '/assets/sounds/welcome.mp3'
-    ]))
+const CACHE_NAME = "carwashdb-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png",
+  "/assets/sound/klik.mp3",
+  "/assets/sound/success.mp3",
+  "/assets/sound/error.mp3",
+  "/assets/sound/welcome.mp3"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Files
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
-
